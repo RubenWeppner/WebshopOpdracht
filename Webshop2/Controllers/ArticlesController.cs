ecde68d6bf1e5fa924e8a0be678198af20c498bb
Ruben Weppner <rweppner3@gmail.com>
2017-06-19 19:58:06 +0200

    Add project files.

---
+++ Webshop2/Controllers/ArticlesController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Webshop2.Models;
+
+namespace Webshop2.Controllers
+{
+    public class ArticlesController : Controller
+    {
+        private WebstoreEntities db = new WebstoreEntities();
+
+		// GET: Articles
+		public ActionResult Index(string sortOrder, string categories, int? id)
+		{
+			ViewBag.NameSort = sortOrder == "Name" ? "Name_desc" : "Name";
+			ViewBag.Categories = new SelectList(db.Categories, "Name", "Name");
+			var articles = db.Articles.Include(a => a.Category).Include(a => a.Supplier);
+			switch (sortOrder)
+			{
+				case "Name":
+					articles = articles.OrderBy(a => a.Name);
+					break;
+				case "Name_desc":
+					articles = articles.OrderByDescending(a => a.Name);
+					break;
+			}
+			if (!String.IsNullOrEmpty(categories))
+				articles = articles.Where(a => a.Category.Name == categories);
+			if (id >= 1)
+			{
+				Order order = new Order();
+				order.ArticleID = id;
+				order.CustomerID = 1;
+				order.Payment = "Cash";
+				order.Amount = 1;
+				db.Orders.Add(order);
+				db.SaveChanges();
+			}
+			return View(articles.ToList());
+		}
+
+        // GET: Articles/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Article article = db.Articles.Find(id);
+            if (article == null)
+            {
+                return HttpNotFound();
+            }
+            return View(article);
+        }
+
+        // GET: Articles/Create
+        public ActionResult Create()
+        {
+            ViewBag.CategoryID = new SelectList(db.Categories, "ID", "Name");
+            ViewBag.SupplierID = new SelectList(db.Suppliers, "ID", "Name");
+            return View();
+        }
+
+        // POST: Articles/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name,Price,Description,Stock,CategoryID,SupplierID")] Article article)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Articles.Add(article);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CategoryID = new SelectList(db.Categories, "ID", "Name", article.CategoryID);
+            ViewBag.SupplierID = new SelectList(db.Suppliers, "ID", "Name", article.SupplierID);
+            return View(article);
+        }
+
+        // GET: Articles/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Article article = db.Articles.Find(id);
+            if (article == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CategoryID = new SelectList(db.Categories, "ID", "Name", article.CategoryID);
+            ViewBag.SupplierID = new SelectList(db.Suppliers, "ID", "Name", article.SupplierID);
+            return View(article);
+        }
+
+        // POST: Articles/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name,Price,Description,Stock,CategoryID,SupplierID")] Article article)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(article).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CategoryID = new SelectList(db.Categories, "ID", "Name", article.CategoryID);
+            ViewBag.SupplierID = new SelectList(db.Suppliers, "ID", "Name", article.SupplierID);
+            return View(article);
+        }
+
+        // GET: Articles/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Article article = db.Articles.Find(id);
+            if (article == null)
+            {
+                return HttpNotFound();
+            }
+            return View(article);
+        }
+
+        // POST: Articles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Article article = db.Articles.Find(id);
+            db.Articles.Remove(article);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
